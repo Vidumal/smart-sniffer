d17effb3ee741d67eb1d7f81033a46b6896e29d1
Punsith Vidumal <punsithvidumal@gmail.com>
2026-02-20 17:56:56 +0530

    Update engine.py

--- engine.py
+++ engine.py
@@ -1,3 +1,15 @@
 import threading
 from scapy.all import sniff, IP, TCP, UDP, Raw
 import queue
+
+class PacketEngine:
+    def __init__(self, ui_callback):
+        self.ui_callback = ui_callback
+        self.running = False
+        self.packet_list = []
+
+    def start_sniffing(self, interface=None):
+        self.running = True
+        self.sniff_thread = threading.Thread(target=self._sniff)
+        self.sniff_thread.daemon = True
+        self.sniff_thread.start()
