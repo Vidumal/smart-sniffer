df7512408e5ebf06a1f95431927e04ca073e5688
Punsith Vidumal <punsithvidumal@gmail.com>
2026-02-20 18:22:58 +0530

    Update intelligence.py

--- intelligence.py
+++ intelligence.py
@@ -1,19 +1,15 @@
 import requests
-import whois 
+import whois
 
 def get_ip_location(ip):
     try:
-        
         response = requests.get(f"https://ipapi.co/{ip}/json/").json()
-        country = response.get("country_name", "Unknown")
-        city = response.get("city", "Unknown")
-        return f"{city}, {country}"
+        return f"{response.get('city', 'Unknown')}, {response.get('country_name', 'Unknown')}"
     except:
         return "Local/Unknown"
 
 def get_org_owner(ip):
     try:
-        
         w = whois.whois(ip)
         return w.org or w.registrar or "Private/Unknown"
     except:
