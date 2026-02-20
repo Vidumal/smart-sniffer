0db832cf6cc14e8150fae07bf29c85ffa208e98f
Punsith Vidumal <punsithvidumal@gmail.com>
2026-02-20 17:44:15 +0530

    Update sniffer.py

--- sniffer.py
+++ sniffer.py
@@ -6,3 +6,14 @@ class SnifferEngine:
     def __init__(self, callback):
         self.callback = callback
         self.running = False
+
+    def start(self):
+        self.running = True
+        self.thread = threading.Thread(target=self._run_sniffing)
+        self.thread.daemon = True
+        self.thread.start()
+    
+    def stop(self):
+        self.running = False
+    
+    
